4bb51352cb3298328a7ad34c05aaaf47b2adbe73
lostzorooo <vatsaljain7576@gmail.com>
2026-06-04 11:39:59 +0530

    fix(backend): remove deprecated next callback from async mongoose hook

--- backend/src/models/File.js
+++ backend/src/models/File.js
@@ -34,13 +34,8 @@ fileSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 fileSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
   
-  try {
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 fileSchema.methods.comparePassword = async function (candidatePassword) {
